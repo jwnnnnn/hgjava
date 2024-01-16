bcdf8610d0263a0529abfa4ab2b1c6dc2052d0a9
admin <admin@DESKTOP-80LD7DS>
2024-01-16 17:33:35 +0900

    ㅁㅁ

--- hgjava/src/com/yedam/todo/Todo0116.java
+++ hgjava/src/com/yedam/todo/Todo0116.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Todo0116 {
 	public static void main(String[] args) {
 		Scanner scn = new Scanner(System.in);
-		
+		ㄹㄹㄹㄹ
 		System.out.println("값을 입력 >>>");
 		String input = scn.nextLine(); //  ==
 		// input.equals("quit");
